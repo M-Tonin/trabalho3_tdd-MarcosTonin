8dabc92fd5dbfab96e618075a3a86287cb6a1c8f
Unknown <marcosvtonin@hotmail.com>
2018-10-03 15:09:46 -0300

    fixs

--- Trabalho3_tdd/teste gtest conta_linha/tests.cpp
+++ Trabalho3_tdd/teste gtest conta_linha/tests.cpp
@@ -2,17 +2,17 @@
 #include "contador.cpp"
 #include <gtest/gtest.h>
 
-ifstream arq,arq1,arq2;
+ifstream arq,arq1,arq2,t0,t1,t2;
  
 TEST(abrirArquivo, teste_abrir) { 
       ASSERT_EQ(1, abrir_arquivo("sdasd.c",arq));	
       ASSERT_EQ(1, abrir_arquivo("sdasf.c",arq1));
-	  ASSERT_EQ(0, abrir_arquivo("sdasf1234154.c",arq2));
+      ASSERT_EQ(0, abrir_arquivo("sdasf1234154.c",arq2));
+      ASSERT_EQ(1,abrir_arquivo("Exemplo24Stack.cpp",t0));
+      ASSERT_EQ(1,abrir_arquivo("Exemplo18Enum2.cpp",t1));
+      ASSERT_EQ(1,abrir_arquivo("Exemplo17Friend.cpp",t2));	
 }
-ifstream t0,t1,t2;
-   abrir_arquivo("Exemplo24Stack.cpp",t0);
-   abrir_arquivo("Exemplo18Enum2.cpp",t1);
-   abrir_arquivo("Exemplo17Friend.cpp",t2);
+  
 TEST(cont_total, contando) { 
       ASSERT_EQ(29, conta_total_linhas(t0));	
       ASSERT_EQ(33, conta_total_linhas(t1));
@@ -33,3 +33,4 @@ int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
